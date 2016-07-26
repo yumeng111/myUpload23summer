e9c9802bce38cb44631ea8b3db9a3a1f58f1235f
Karoly Banicz <Karoly.Banicz@cern.ch>
2016-07-26 13:14:20 +0000

    Version 12.09.00 EmuPageOne: Zooming working in Chrome and Firefox. Plot TCDS hard reset count instead of trigger rate.

--- emu/base/include/emu/base/version.h
+++ emu/base/include/emu/base/version.h
@@ -5,7 +5,7 @@
 
 namespace emubase {
 	const std::string package     = "emubase";
-	const std::string versions    = "12.08.00";
+	const std::string versions    = "12.09.00";
 	const std::string description = "Emu (CSC) applications' base classes";
 
 	const std::string summary     = "emu/base";

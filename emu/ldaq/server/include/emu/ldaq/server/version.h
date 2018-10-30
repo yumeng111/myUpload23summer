893d05412e81a14830e92070ccc20e05403e30aa
Karoly Banicz <Karoly.Banicz@cern.ch>
2018-10-30 09:25:30 +0000

    Version 14.03.00

--- emu/ldaq/server/include/emu/ldaq/server/version.h
+++ emu/ldaq/server/include/emu/ldaq/server/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqserver {
 	const std::string package     = "emudaqserver";
-	const std::string versions    = "14.01.00";
+	const std::string versions    = "14.03.00";
 	const std::string description = "Emu (CSC) DAQ servers";
 
 	const std::string summary     = "emu/ldaq/server";

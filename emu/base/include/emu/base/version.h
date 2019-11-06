9d63a055dcf8ff6bf9b9eff0a99acadcdb60b167
Jinghua Liu <liu@ctrl-s2g18-15-01.cms>
2019-11-06 18:15:04 +0100

    version 14.06

--- emu/base/include/emu/base/version.h
+++ emu/base/include/emu/base/version.h
@@ -5,7 +5,7 @@
 
 namespace emubase {
 	const std::string package     = "emubase";
-	const std::string versions    = "14.05.00";
+	const std::string versions    = "14.06.00";
 	const std::string description = "Emu (CSC) applications' base classes";
 
 	const std::string summary     = "emu/base";

bd1ee0a5ce8d40d79bb5ad49a344eee3ad0b6b57
liu <Jinghua.Liu@cern.ch>
2020-07-10 08:16:07 -0500

    fix LVDB5 3V current correction

--- emu/emuDCS/PeripheralApps/src/common/EmuPeripheralCrateConfig_DMB.cc
+++ emu/emuDCS/PeripheralApps/src/common/EmuPeripheralCrateConfig_DMB.cc
@@ -4417,7 +4417,7 @@ void EmuPeripheralCrateConfig::DMBStatus(xgi::Input * in, xgi::Output * out )
       if(nadcs==5 && cversion>1)  /* for chambers with 5 (x)DCFEEBs */ 
       {
          if(i>=vstart) fvalue[i] = fvalue[i]*1.03;  /* correction for all voltages */
-         else if(i<15 && (i%3)==1) fvalue[i] = fvalue[i]*2;  /* DCFEB 4V currents multiply by 2 */    
+         else if(i<15 && (i%3)==0) fvalue[i] = fvalue[i]*2;  /* DCFEB 3V currents multiply by 2 */    
       }
   }
   int chn2pos[8];

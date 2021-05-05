327e886b9a4a1aeea6816d5c0aac7fcae6dbabda
Victor Barashko <Victor.Barachko@cern.ch>
2021-05-05 02:10:18 +0200

    Added missing include/emu/dqm/version.h for RPM building. Updated cmssw/dist/CMSSW_unpacker_postLS1.tgz unpacking distribution for LS2 commissioning

---
+++ emu/dqm/include/emu/dqm/version.h
@@ -0,0 +1,22 @@
+#ifndef __emu_dqm_version_h
+#define __emu_dqm_version_h
+
+#include "config/PackageInfo.h"
+
+namespace emudqmcscmonitor 
+{
+
+const std::string project     =	"emu";
+const std::string package     = "emudqm";
+const std::string versions    = "15.00.00";
+const std::string description = "EMU CSC Data Monitoring Modules";
+const std::string link = "http://www.phys.ufl.edu/cms/emu/dqm";
+const std::string authors = "Victor Barashko <barvic@ufl.edu>";
+const std::string summary = "Emu CSC DQM Monitoring Modules";
+
+config::PackageInfo getPackageInfo();
+void checkPackageDependencies() throw (config::PackageInfo::VersionException);
+std::set<std::string, std::less<std::string> > getPackageDependencies();
+};
+
+#endif 

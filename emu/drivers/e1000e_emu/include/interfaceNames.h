2af3cb72e03a0ad330b3e0a6f75a94beb97dfaab
K. Banicz <karoly.banicz@cern.ch>
2021-05-11 15:18:35 +0200

    Revert to old interface names. (Not changed in old PCs.)

--- emu/drivers/e1000e_emu/include/interfaceNames.h
+++ emu/drivers/e1000e_emu/include/interfaceNames.h
@@ -1,4 +1,4 @@
-#define __ETH2__ "p2p1"
-#define __ETH3__ "p2p2"
-#define __ETH4__ "p3p1"
-#define __ETH5__ "p3p2"
+#define __ETH2__ "p1p1"
+#define __ETH3__ "p1p2"
+#define __ETH4__ "p2p1"
+#define __ETH5__ "p2p2"
